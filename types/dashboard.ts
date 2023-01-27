1d42d6f18fd6aa7c50d7bca7d2047223f5f945a0
alwar <alwaromoreno@gmail.com>
2023-01-27 02:05:19 +0100

    input types

--- types/dashboard.ts
+++ types/dashboard.ts
@@ -4,7 +4,17 @@ export interface InputProps {
   type: HTMLInputTypeAttribute
   name: string
   label?: string
+  placeholder?: string
+}
+export interface InputNumberProps extends Omit<InputProps, 'type'> {
+  currency?: boolean
+}
+export interface InputCheckboxProps extends Omit<InputProps, 'type'> {
+  text: string
+}
+
+export interface textAreaProps {
+  name: string
   placeholder: string
-  onChange: any
-  value: string | number
+  label?: string
 }

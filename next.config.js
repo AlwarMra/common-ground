a3d10c542eab06965bfb92e5b4bd9b378c49623c
alwar <alwaromoreno@gmail.com>
2023-02-07 17:22:03 +0100

    next config for image

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
     defaultLocale: 'es',
     // localeDetection: false,
   },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
